99657a5e861ebf4d71c6ceaa0aec4e793d2ea531
zft666zft <2810458566@qq.com>
2025-04-14 15:29:10 +0100

    Initial commit

---
+++ test/photo-gallery.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PhotoGallery from '../lib/photo-gallery-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/photo-gallery-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PhotoGallery.PhotoGalleryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
